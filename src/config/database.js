d615ab752828dc07af5c516ba175073e195407d5
vlimap <valtemir.procopio@gmail.com>
2026-07-14 16:53:01 -0300

    add: integrando com banco postgres

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,20 @@
-// Este arquivo simula um banco de dados usando um array em memoria.
-//
 // Em um projeto real, aqui normalmente fariamos a conexao com um banco
 // como MySQL, PostgreSQL, MongoDB, SQLite, entre outros.
-//
-// Como este projeto e didatico, vamos guardar os alunos neste array.
-// Importante: quando o servidor for reiniciado, os dados serao apagados.
-const alunos = [];
+import pg from 'pg'
+import dotenv from "dotenv"
+dotenv.config()
 
-// Exporta o array para que outros arquivos possam cadastrar, listar,
-// editar e excluir alunos.
-export default alunos;
+const { Pool } = pg
+
+const conexao = new Pool({
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+})
+
+console.log(await conexao.query('SELECT 1+1'))
+
+export default conexao;
+
